58b7d7b8654a2ffca30aeb2b7c248de9d7530dc9
Mitchell Harvey <mitch.k.harvey@gmail.com>
2018-12-30 15:43:55 +1100

    problem 4 complete

--- python/problem4.py
+++ python/problem4.py
@@ -1,36 +1,27 @@
 # project euler problem 4
 
-numberOfDigits = int(input("enter the number of digits of the two numbers that the palindrome \nshould be made from (eg. 3 for a three digit number): "))
-
-temp = ""
-for i in range(numberOfDigits):
-	temp = temp + "9"
-
-
-maxNumber = int(temp)
-
-print(maxNumber)
-
-palindrome = False
-
 def palindromeCheck(n):
-	n = str(n)
+
+	try:
+		n = str(n)
+	except:
+		print("hmm")
+	
 	if n == n[::-1]:
-		palindrome = True
-		n = int(n)
 		return True
 	else:
 		return False
 
 
-while(palindrome != True):
-	for i in range(1, 11):
-		for n1 in range(maxNumber-(i*100), maxNumber):
-			for n2 in range(maxNumber-(i*100), maxNumber):
-				num = n1 * n2
-				if palindromeCheck(num):
-					largestPalindrome = num	
-	
+def findNumber():
+	maxNumber = 0
+	for i in range(999):
+		for j in range(999):
+			number = i*j
+			if number > maxNumber and palindromeCheck(number):
+				maxNumber = number
+	return maxNumber
 
-print(n)
 
+
+print(findNumber())
